5fa9d4e5305b0087ced4b92422fce8dba106afd4
Berkay Antmen <c.berkayantmen@gmail.com>
2021-01-17 22:54:37 -0500

    Add more test cases

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,24 +1,49 @@
 set -e
 
-assert_count_occurrence() {
-    out=$1
-    pattern=$2
-    count=$3
-    found=$(echo "$out" | grep -c "$pattern")
-    if [ "$count" -ne "$found" ]; then
-        echo "Expected $count counts but found $found for: $pattern"
+assert_equal() {
+    expected=$1
+    observed=$2
+    if [ "$expected" != "$observed" ]; then
+        echo "Test failed. Expected:"
+        echo "$expected"
+        echo "Observed:"
+        echo "$observed"
         exit 1 
     fi
 }
 
 cd bins
 
-out=$(./procman_linux_amd64 echo testing | head -n6)
+# No flags
+expected="Running process:  [echo testing]
+testing
+Process stopped without error
+testing
+Process stopped without error
+testing"
+assert_equal "$expected" "$(./procman_linux_amd64 echo testing | head -n6)"
 
-assert_count_occurrence "$out" "^Running process:  \[echo testing\]$" 1
-assert_count_occurrence "$out" '^testing$' 3
-assert_count_occurrence "$out" '^Process stopped without error' 2
+# mem flag
+expected="Running process:  [echo testing]
+With memory threshold: 50.00 percent
+testing
+Process stopped without error
+testing
+Process stopped without error
+testing"
+assert_equal "$expected" "$(./procman_linux_amd64 -mem 50 echo testing | head -n7)"
+
+# logfile flag with empty error file
+temp_file=$(mktemp)
+expected="Running process:  [echo testing]
+Logging command stdout to '$temp_file' and stderr to '$temp_file.error'
+Process stopped without error"
+assert_equal "$expected" "$(./procman_linux_amd64 -logfile $temp_file echo testing | head -n3)"
+logfile_expected="testing
+testing"
+assert_equal "$logfile_expected" "$(cat $temp_file)"
+logfile_err_out=$(cat $temp_file.error)
+assert_equal "" "$logfile_err_out"
 
 cd - > /dev/null
-
 echo "Test success!"
